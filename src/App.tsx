b7d9c63416aa7b79e2cb441b9afb1800ca822ede
YuriiRG <yuriirg@tutanota.com>
2022-12-15 20:32:34 +0100

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <h1 className="text-3xl font-bold first-letter:text-9xl">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
