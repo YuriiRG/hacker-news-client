0b49c8298d2d02f5a2bc6a228536d74839c99e14
YuriiRG <yuriirg@tutanota.com>
2022-12-16 14:47:12 +0100

    Install Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,29 @@ import { lazy, Suspense } from 'react';
 import { Route, Switch } from 'wouter';
 import NavBar from './components/NavBar';
 import Loader from './routes/Loader';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const Root = lazy(() => import('./routes/Root'));
-const Page1 = lazy(() => import('./routes/Page1'));
 const NotFound = lazy(() => import('./routes/NotFound'));
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <NavBar />
-      <Suspense fallback={<Loader />}>
-        <Switch>
-          <Route path='/'>
-            <Root />
-          </Route>
-          <Route path='/page1'>
-            <Page1 />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='flex flex-col min-h-screen'>
+        <NavBar />
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path='/'>
+              <Root />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
+      </div>
+    </QueryClientProvider>
   );
 }
--- src/routes/Page1.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page1() {
-  return <>Page 1 data</>;
-}
