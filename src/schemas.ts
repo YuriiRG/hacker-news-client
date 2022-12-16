42fd7b7409d93126c6a103a114f0827357f39193
YuriiRG <yuriirg@tutanota.com>
2022-12-16 16:23:08 +0100

    Create a few necessary Zod schemas

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,1 +1,36 @@
-export {};
+import { z } from 'zod';
+
+export const storySchema = z.object({
+  id: z.number(),
+  by: z.string(),
+  descendants: z.number(),
+  kids: z.array(z.number()),
+  score: z.number(),
+  time: z.number(),
+  title: z.string(),
+  type: z.literal('story'),
+  url: z.string()
+});
+
+export const commentSchema = z.object({
+  id: z.number(),
+  by: z.string(),
+  kids: z.array(z.number()),
+  parent: z.number(),
+  text: z.string(),
+  time: z.number(),
+  type: z.literal('comment')
+});
+
+export const userSchema = z.object({
+  id: z.string(),
+  about: z.string(),
+  created: z.number(),
+  karma: z.number(),
+  submitted: z.array(z.number())
+});
+
+export const itemSchema = z.discriminatedUnion('type', [
+  storySchema,
+  commentSchema
+]);
