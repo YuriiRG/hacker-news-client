71cfddffdfc92c62d86e47d405db8cc12a96cd28
YuriiRG <yuriirg@tutanota.com>
2022-12-16 11:31:55 +0100

    Install Rome as a formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold first-letter:text-9xl">
-      Hello world!
-    </h1>
-  )
+    <h1 className="text-3xl font-bold first-letter:text-9xl">Hello world!</h1>
+  );
 }
