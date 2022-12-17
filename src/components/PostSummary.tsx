41b1cdcccf24ab89a13cb187be010676632c9436
YuriiRG <yuriirg@tutanota.com>
2022-12-17 17:48:19 +0100

    Create a skeleton placeholder for data

--- src/components/PostSummary.tsx
+++ src/components/PostSummary.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import fetcher from '../helpers/fetcher';
-import Loader from '../routes/Loader';
 import { postSchema } from '../schemas';
 
 export default function PostSummary({ id }: { id: number }) {
@@ -10,24 +9,27 @@ export default function PostSummary({ id }: { id: number }) {
       fetcher(
         `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
         postSchema
-      )
+      ),
+    keepPreviousData: true
   });
   if (isError) {
     return <>Error.</>;
   }
   if (isLoading) {
-    return <Loader />;
+    return (
+      <div className='flex gap-1 p-1'>
+        <div className='w-10 h-6 bg-gray-600 rounded-lg'></div>
+        <div className='w-10 h-6 bg-gray-600 rounded-lg'></div>
+        <div className='w-96 h-6 bg-gray-600 rounded-lg'></div>
+      </div>
+    );
   }
 
   return (
-    <div className='p-1'>
-      <div className='flex gap-1'>
-        <div className='w-10'>{data.score}</div>
-        {data.type === 'story' && (
-          <div className='w-10'>{data.descendants}</div>
-        )}
-        <a href={data.url}>{data.title}</a>
-      </div>
+    <div className='flex gap-1 p-1'>
+      <div className='w-10'>{data.score}</div>
+      {data.type === 'story' && <div className='w-10'>{data.descendants}</div>}
+      <a href={data.url}>{data.title}</a>
     </div>
   );
 }
